cfa551181e728e4610ab5ac6d1b1a4c080525bfb
povnurb <ing.eduardo.sae.tmx@gmail.com>
2023-02-12 18:48:00 -0600

    primer commit

---
+++ src/pages/admin/lalo/Lalo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="lalo">
+    LALO
+
+    <div class="row row-equal">
+      <div class="flex xs12 lg6">
+        <dashboard-tabs @submit="addAddressToMap" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  /*import DashboardCharts from './DashboardCharts.vue'
+  import DashboardInfoBlock from './DashboardInfoBlock.vue'
+  import DashboardTabs from './DashboardTabs.vue'
+  import DashboardMap from './DashboardMap.vue'*/
+
+  const dashboardMap = ref()
+
+  function addAddressToMap({ city, country }: { city: { text: string }; country: string }) {
+    dashboardMap.value.addAddress({ city: city.text, country })
+  }
+</script>
+
+<style lang="scss">
+  .row-equal .flex {
+    .va-card {
+      height: 100%;
+    }
+  }
+
+  .dashboard {
+    .va-card {
+      margin-bottom: 0 !important;
+      &__title {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+</style>
